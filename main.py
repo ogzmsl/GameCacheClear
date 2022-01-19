4368e88c3bb7ae16dba5fda101cda6fbb65abd02
Oğuz Muslu <oguzzmuslu@gmail.com>
2022-01-19 07:59:10 +0300

    Add to schedule define in function comments

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import glob
 import schedule
 import time
 
-
+# Define Path
 def job():
     files = glob.glob('C:/Users/Oguz/Desktop/New folder/*')
     for f in files:
         os.remove(f)
 
-
+# Date and Schedule
 schedule.every().friday.at("07:01").do(job)
 schedule.every().friday.at("17:00").do(job)
 
