c23754fe5dd1376cde809ffb73340b8cf0c9b6bd
Oğuz Muslu <oguzzmuslu@gmail.com>
2022-01-19 07:15:15 +0300

    Add to schedule define in function

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import datetime
 import os
 import glob
+import schedule
+import time
 
 
-files = glob.glob('C:/Users/Oguz/Desktop/New folder/*')
-for f in files:
-    os.remove(f)
+def job():
+    files = glob.glob('C:/Users/Oguz/Desktop/New folder/*')
+    for f in files:
+        os.remove(f)
 
-"""""
-DayL = ['Mon', 'Tues', 'Wednes', 'Thurs', 'Fri', 'Satur', 'Sun']
-date = DayL[datetime.date(2022, 1, 12).weekday()] + 'day'
-print(date)
-"""""
+
+schedule.every().friday.at("07:01").do(job)
+schedule.every().friday.at("17:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
